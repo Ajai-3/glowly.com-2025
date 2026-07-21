2cd9907a4963b08a80418c9b324b03afe36cd7ca
Ajai-3 <ajaipjayan@gmail.com>
2026-07-21 08:11:36 +0530

    fix: resolve 413 request entity too large by ordering express body parser limit first

--- server.js
+++ server.js
@@ -58,12 +58,9 @@ cron.schedule("* * * * *",async () => {
 app.set("view engine", "ejs");
 app.use(nocache());
 app.use(cookieParser()); 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true })); 
 
-
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 // Passport session initialization
